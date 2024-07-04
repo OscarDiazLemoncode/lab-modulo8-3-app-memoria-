c832067a0e2a1aef2c5a79ebc8f8a6cfa0d79a64
Oscar <trabajos1379@gmail.com>
2024-07-04 20:40:39 +0200

    2

--- src/main.ts
+++ src/main.ts
@@ -20,32 +20,87 @@ const barajarCartas = () => {
   console.log(barajados);
   return barajados;
 };
-
-const eventos = () => {
-  // Añadimos clase .voltear al click
-  const img = document.querySelector('.url_carta');
-  const card = document.querySelector('.card');
-  if (
-    card !== null &&
-    card !== undefined &&
-    card instanceof HTMLDivElement &&
-    img !== null &&
-    img !== undefined &&
-    img instanceof HTMLImageElement
-  ) {
-    card.addEventListener('click', () => {
-      setTimeout(() => {
-        // Cambiamos src de img
-        img.setAttribute(
-          'src',
-          'https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/1.png?raw=true'
-        );
-      }, 300);
-      // Añadimos clase .voltear
-      card.classList.add('voltear');
+/* const eventos = () => {
+  const cards = document.querySelectorAll('.card');
+  cards.forEach((card) => {
+    card.addEventListener('click', (e) => {
+      const elementoClickado = e.target;
+      if (elementoClickado instanceof HTMLImageElement) {
+        // El elemento clicado es una <img>
+        console.log('Elemento <img> clicado:', elementoClickado);
+      } else {
+        // Si el clic no fue directamente en una <img>, busca dentro del .card
+        const imgElement = card.querySelector('img');
+        if (imgElement) {
+          console.log('Elemento <img> dentro del .card:', imgElement);
+        }
+      }
     });
-  }
+  });
+}; */
+const eventos = () => {
+  // Añadimos clase .voltear
+  const cards = document.querySelectorAll('.card');
+  //const imgCard = document.querySelector('.card img');
+  cards.forEach((card, index) => {
+    // img del elemento clickado
+    const imgElemento = card.querySelector('.card img');
+    card.addEventListener('click', () => {
+      //const elementoClickado = e.target;
+      if (
+        card !== null &&
+        card !== undefined &&
+        card instanceof HTMLDivElement &&
+        imgElemento !== null &&
+        imgElemento !== undefined &&
+        imgElemento instanceof HTMLImageElement
+      ) {
+        // Añadimos clase .voltear
+        card.classList.add('voltear');
+        //console.log(elementoClickado);
+        console.log(card);
+        console.log(imgElemento);
+        console.log(index);
+        switch (index) {
+          case 0:
+            console.log('click en el 1º');
+            imgElemento.src =
+              'https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/3.png?raw=true';
+
+            break;
+          case 1:
+            console.log('click en el 2º');
+            imgElemento.src =
+              'https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/4.png?raw=true';
+            break;
+          default:
+            break;
+        }
+      } else {
+        console.warn('No se ha modificado el src de la imagen');
+      }
+    });
+  });
 };
+
+/* const cambiarSrcImg = () => {
+  const imgs = document.querySelectorAll('.card img');
+  let src: string = '';
+  imgs.forEach((img) => {
+    if (img !== null && img !== undefined && img instanceof HTMLImageElement) {
+      src = img.src;
+      console.log(src);
+    }
+  });
+  return src;
+}; */
+
+/* imgs.forEach((img) => {
+      img.setAttribute(
+        'src',
+        'https://github.com/Lemoncode/fotos-ejemplos/blob/main/memo/3.png?raw=true'
+      );
+    }); */
 
 const handlerButton = () => {
   const button = document.querySelector('.barajar');
@@ -64,3 +119,22 @@ document.addEventListener('DOMContentLoaded', () => {
   eventos();
   handlerButton();
 });
+
+/* const eventos = () => {
+  const cards = document.querySelectorAll('.card');
+  cards.forEach((card) => {
+    card.addEventListener('click', (e) => {
+      const elementoClickado = e.target;
+      if (elementoClickado instanceof HTMLImageElement) {
+        // El elemento clicado es una <img>
+        console.log('Elemento <img> clicado:', elementoClickado);
+      } else {
+        // Si el clic no fue directamente en una <img>, busca dentro del .card
+        const imgElement = card.querySelector('img');
+        if (imgElement) {
+          console.log('Elemento <img> dentro del .card:', imgElement);
+        }
+      }
+    });
+  });
+}; */
