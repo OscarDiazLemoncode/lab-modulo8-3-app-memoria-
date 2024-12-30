f217eba9f16e2c6ff4afccf7976f42a209bb17f8
Oscar <trabajos1379@gmail.com>
2024-12-30 20:26:50 +0100

    refactor motor.ts, integrado parejaEncontrada y parejaNoEncontrada, empezarPartida

--- src/motor.ts
+++ src/motor.ts
@@ -1,22 +1,92 @@
-import { cartas, Carta, tablero, Tablero } from './model';
+import { /* cartas, */ Carta, tablero, Tablero } from './model';
 //Barajamos array duplicado
-const barajarCartas = (cartas: Carta[]): Carta[] => {
-  const barajadoColeccionDeCartas = cartas.sort(() => Math.random() - 0.5);
+const barajarCartas = (cartasTablero: Carta[]): Carta[] => {
+  const barajadoColeccionDeCartas = cartasTablero.sort(
+    () => Math.random() - 0.5
+  );
   return barajadoColeccionDeCartas;
 };
-export const cartasBarajadas = barajarCartas(cartas);
-// export const cartasBarajadas = barajarCartas(tablero.cartas);
+// export const cartasBarajadas = barajarCartas(cartas);
+export const cartasBarajadas = barajarCartas(tablero.cartas);
 console.table(cartasBarajadas);
 
 // Una carta se puede voltear si no está encontrada y no está ya volteada, o no hay dos cartas ya volteadas
+// const sePuedeVoltearLaCarta = (tablero: Tablero, indice: number): boolean => {
+//   const carta = tablero.cartas[indice];
+//   const cartaNoEncontradaYNoVolteada = !carta.encontrada && !carta.estaVuelta;
+//   const noHayDosCartasVolteadas =
+//     tablero.indiceCartaVolteadaA === undefined ||
+//     tablero.indiceCartaVolteadaB === undefined;
+
+//   console.log();
+//   return cartaNoEncontradaYNoVolteada && noHayDosCartasVolteadas;
+// };
+
+// Una carta se puede voltear si no está encontrada y no está ya volteada, o no hay dos cartas ya volteadas
 const sePuedeVoltearLaCarta = (tablero: Tablero, indice: number): boolean => {
+  if (indice < 0 || indice >= tablero.cartas.length) {
+    return false;
+  }
+
   const carta = tablero.cartas[indice];
   const cartaNoEncontradaYNoVolteada = !carta.encontrada && !carta.estaVuelta;
   const noHayDosCartasVolteadas =
     tablero.indiceCartaVolteadaA === undefined ||
     tablero.indiceCartaVolteadaB === undefined;
+
   return cartaNoEncontradaYNoVolteada && noHayDosCartasVolteadas;
 };
+
+// const voltearLaCarta = (tablero: Tablero, indice: number): void => {
+//   if (sePuedeVoltearLaCarta(tablero, indice)) {
+//     const carta = tablero.cartas[indice];
+//     carta.estaVuelta = true;
+
+//     if (tablero.indiceCartaVolteadaA === undefined) {
+//       tablero.indiceCartaVolteadaA = indice;
+//       tablero.estadoPartida = 'UnaCartaLevantada';
+//     } else if (tablero.indiceCartaVolteadaB === undefined) {
+//       tablero.indiceCartaVolteadaB = indice;
+//       tablero.estadoPartida = 'DosCartasLevantadas';
+
+//       if (
+//         tablero.indiceCartaVolteadaA !== undefined &&
+//         tablero.indiceCartaVolteadaB !== undefined
+//       ) {
+//         if (
+//           sonPareja(
+//             tablero.indiceCartaVolteadaA,
+//             tablero.indiceCartaVolteadaB,
+//             tablero
+//           )
+//         ) {
+//           parejaEncontrada(
+//             tablero,
+//             tablero.indiceCartaVolteadaA,
+//             tablero.indiceCartaVolteadaB
+//           );
+//         } else {
+//           setTimeout(() => {
+//             if (
+//               tablero.indiceCartaVolteadaA !== undefined &&
+//               tablero.indiceCartaVolteadaB !== undefined
+//             ) {
+//               parejaNoEncontrada(
+//                 tablero,
+//                 tablero.indiceCartaVolteadaA,
+//                 tablero.indiceCartaVolteadaB
+//               );
+//             }
+//           }, 1000);
+//         }
+//       }
+
+//       /* tablero.indiceCartaVolteadaA = undefined;
+//       tablero.indiceCartaVolteadaB = undefined;
+//       tablero.estadoPartida = 'CeroCartasLevantadas'; */
+//     }
+//   }
+// };
 
 const voltearLaCarta = (tablero: Tablero, indice: number): void => {
   if (sePuedeVoltearLaCarta(tablero, indice)) {
@@ -26,9 +96,11 @@ const voltearLaCarta = (tablero: Tablero, indice: number): void => {
     if (tablero.indiceCartaVolteadaA === undefined) {
       tablero.indiceCartaVolteadaA = indice;
       tablero.estadoPartida = 'UnaCartaLevantada';
+      console.log('UNA CARTA LEVANTADA');
     } else if (tablero.indiceCartaVolteadaB === undefined) {
       tablero.indiceCartaVolteadaB = indice;
       tablero.estadoPartida = 'DosCartasLevantadas';
+      console.log('DOS CARTAS LEVANTADAS');
 
       if (
         tablero.indiceCartaVolteadaA !== undefined &&
@@ -46,25 +118,30 @@ const voltearLaCarta = (tablero: Tablero, indice: number): void => {
             tablero.indiceCartaVolteadaA,
             tablero.indiceCartaVolteadaB
           );
+          // Restablecer el estado para permitir seguir jugando
+          tablero.indiceCartaVolteadaA = undefined;
+          tablero.indiceCartaVolteadaB = undefined;
+          // tablero.estadoPartida = 'CeroCartasLevantadas';
+          console.warn(tablero.estadoPartida);
         } else {
           setTimeout(() => {
-            if (
-              tablero.indiceCartaVolteadaA !== undefined &&
-              tablero.indiceCartaVolteadaB !== undefined
-            ) {
-              parejaNoEncontrada(
-                tablero,
-                tablero.indiceCartaVolteadaA,
-                tablero.indiceCartaVolteadaB
-              );
-            }
+            parejaNoEncontrada(
+              tablero,
+              tablero.indiceCartaVolteadaA!,
+              tablero.indiceCartaVolteadaB!
+            );
+            document
+              .querySelector(`[data-indice="${tablero.indiceCartaVolteadaA}"]`)
+              ?.classList.remove('voltear');
+            document
+              .querySelector(`[data-indice="${tablero.indiceCartaVolteadaB}"]`)
+              ?.classList.remove('voltear');
+            tablero.indiceCartaVolteadaA = undefined;
+            tablero.indiceCartaVolteadaB = undefined;
+            tablero.estadoPartida = 'CeroCartasLevantadas';
           }, 1000);
         }
       }
-
-      /* tablero.indiceCartaVolteadaA = undefined;
-      tablero.indiceCartaVolteadaB = undefined;
-      tablero.estadoPartida = 'CeroCartasLevantadas'; */
     }
   }
 };
@@ -74,11 +151,9 @@ const sonPareja = (
   indiceB: number,
   tablero: Tablero
 ): boolean => {
-  console.warn(
-    tablero.cartas[indiceA].idFoto === tablero.cartas[indiceB].idFoto
-  );
   return tablero.cartas[indiceA].idFoto === tablero.cartas[indiceB].idFoto;
 };
+
 const parejaEncontrada = (
   tablero: Tablero,
   indiceA: number,
@@ -86,7 +161,6 @@ const parejaEncontrada = (
 ): void => {
   tablero.cartas[indiceA].encontrada = true;
   tablero.cartas[indiceB].encontrada = true;
-  // Aquí puedes añadir lógica adicional, como actualizar el estado del juego o mostrar un mensaje
   console.warn('¡Pareja encontrada!');
 };
 
@@ -97,58 +171,108 @@ const parejaNoEncontrada = (
 ): void => {
   tablero.cartas[indiceA].estaVuelta = false;
   tablero.cartas[indiceB].estaVuelta = false;
-  // Aquí puedes añadir lógica adicional, como actualizar el estado del juego o mostrar un mensaje
+  console.warn('Pareja NO encontrada');
 };
 
 export const esPartidaCompleta = (tablero: Tablero): boolean => {
   return tablero.cartas.every((carta) => carta.encontrada);
 };
 
-export const crearTableroInicial = (
-  cartasBarajadas: Carta[],
-  tablero: Tablero
-): Carta[] => {
-  const gridCartas = document.querySelector('.grid_cards');
-  let barajaDeCartasInicial: Carta[] = [];
+// Tablero con cartas en el DOM
+// export const crearTableroInicial = (cartasTablero: Carta[]): Carta[] => {
+//   const gridCartas = document.querySelector('.grid_cards');
+//   if (
+//     gridCartas !== null &&
+//     gridCartas !== undefined &&
+//     gridCartas instanceof HTMLDivElement
+//   ) {
+//     return cartasTablero.map((carta, indice) => {
+//       const dataIndiceArray = indice.toString();
+//       const divCarta = crearDiv();
+//       divCarta.classList.add('card');
+//       const divCartaPosterior = crearDiv();
+//       divCartaPosterior.classList.add('back');
+//       const imgCarta = crearImg();
+//       // Creamos estructura de cada card
+//       gridCartas.appendChild(divCarta);
+//       divCarta.appendChild(divCartaPosterior);
+//       divCartaPosterior.appendChild(imgCarta);
 
+//       divCarta.addEventListener('click', () => {
+//         divCarta.setAttribute('data-indice', dataIndiceArray);
+//         imgCarta.setAttribute('data-indice-image', dataIndiceArray);
+//         if (
+//           sePuedeVoltearLaCarta(tablero, indice) &&
+//           tablero.estadoPartida !== 'PartidaNoIniciada'
+//         ) {
+//           voltearLaCarta(tablero, indice);
+//           imgCarta.src = carta.imagen;
+//           divCarta.classList.add('voltear');
+//           /*  */
+
+//           console.warn(carta);
+//           /*  */
+//           console.warn(tablero);
+
+//           // Comprobar si la partida está completa
+//           if (esPartidaCompleta(tablero)) {
+//             console.log('¡Partida completa!');
+//           }
+//         }
+//       });
+//       return carta;
+//     });
+//   } else {
+//     throw new Error('No se ha creado la estructura de las cartas');
+//   }
+// };
+
+export const crearTableroInicial = (cartasTablero: Carta[]): Carta[] => {
+  const gridCartas = document.querySelector('.grid_cards');
   if (
     gridCartas !== null &&
     gridCartas !== undefined &&
     gridCartas instanceof HTMLDivElement
   ) {
-    cartasBarajadas.forEach((carta, indice) => {
+    return cartasTablero.map((carta, indice) => {
       const dataIndiceArray = indice.toString();
-      const divCard = crearDiv();
-      divCard.classList.add('card');
-      const divBack = crearDiv();
-      divBack.classList.add('back');
-      const imgCard = crearImg();
+      const divCarta = crearDiv();
+      divCarta.classList.add('card');
+      divCarta.setAttribute('data-indice', dataIndiceArray);
+      const divCartaPosterior = crearDiv();
+      divCartaPosterior.classList.add('back');
+      const imgCarta = crearImg();
+      divCarta.appendChild(divCartaPosterior);
+      divCartaPosterior.appendChild(imgCarta);
+      gridCartas.appendChild(divCarta);
 
-      // Creamos estructura de cada card
-      gridCartas.appendChild(divCard);
-      divCard.appendChild(divBack);
-      divBack.appendChild(imgCard);
-      divCard.addEventListener('click', () => {
-        if (sePuedeVoltearLaCarta(tablero, indice)) {
-          divCard.setAttribute('data-indice', dataIndiceArray);
-          imgCard.setAttribute('data-indice-image', dataIndiceArray);
-          voltearLaCarta(tablero, indice);
-          imgCard.src = carta.imagen;
-          divCard.classList.add('voltear');
-          console.warn(tablero);
-          // Comprobar si la partida está completa
-          if (esPartidaCompleta(tablero)) {
-            console.log('¡Partida completa!');
+      divCarta.addEventListener('click', () => {
+        if (tablero.estadoPartida !== 'PartidaNoIniciada') {
+          if (tablero.estadoPartida === 'PartidaCompleta') {
+            return;
           }
+          console.warn(tablero.estadoPartida);
+          console.warn(carta);
+          const indice = parseInt(divCarta.getAttribute('data-indice')!);
+          if (sePuedeVoltearLaCarta(tablero, indice)) {
+            voltearLaCarta(tablero, indice);
+            imgCarta.src = carta.imagen;
+            divCarta.classList.add('voltear');
+
+            // Comprobar si la partida está completa
+            if (esPartidaCompleta(tablero)) {
+              tablero.estadoPartida = 'PartidaCompleta';
+              console.log('¡Partida completa!');
+            }
+          }
+        } else {
+          divCarta.classList.remove('voltear');
         }
       });
-
-      barajaDeCartasInicial.push(carta);
+      return carta;
     });
-  } else {
-    throw new Error('No se ha creado la estructura de las cartas');
   }
-  return barajaDeCartasInicial;
+  return [];
 };
 
 export const iniciaPartida = (tablero: Tablero): void => {
@@ -160,18 +284,6 @@ export const iniciaPartida = (tablero: Tablero): void => {
   tablero.indiceCartaVolteadaB = undefined;
   tablero.estadoPartida = 'CeroCartasLevantadas';
 };
-
-// Obtener indice de cada carta
-/* const indiceCarta = (cartasBarajadas: Carta[]): number[] => {
-  let indices = cartasBarajadas.map((_carta, index) => {
-    //console.log(`index${index}`);
-    return index;
-  });
-  //console.log(`index${indices}`);
-  return indices;
-};
-const indiceCard = indiceCarta(cartasBarajadas);
-console.log(indiceCard); */
 
 const crearDiv = (): HTMLDivElement => {
   const div = document.createElement('div');
@@ -200,9 +312,10 @@ export const empezarPartida = (): void => {
   if (btnEmpezarPartida && btnEmpezarPartida instanceof HTMLButtonElement) {
     btnEmpezarPartida.addEventListener('click', () => {
       tablero.estadoPartida = 'CeroCartasLevantadas';
+      console.warn(tablero.estadoPartida);
       habilitarCartas();
       reiniciarPartida();
-      console.warn(tablero);
+      iniciaPartida(tablero);
     });
   } else {
     throw new Error('No se ha modificado el estado de la partida');
--- src/ui.ts
+++ src/ui.ts
@@ -1,9 +1,14 @@
 import { tablero } from './model';
-import { cartasBarajadas, crearTableroInicial, empezarPartida } from './motor';
+import {
+  /*  cartasBarajadas, */ crearTableroInicial,
+  empezarPartida,
+} from './motor';
 
 document.addEventListener('DOMContentLoaded', () => {
   // eventos();
-  crearTableroInicial(cartasBarajadas, tablero);
+  // crearTableroInicial(cartasBarajadas, tablero);
+  crearTableroInicial(tablero.cartas);
   // tablero.estadoPartida === 'PartidaNoIniciada' ? empezarPartida() : null;
   empezarPartida();
+  console.warn(tablero.estadoPartida);
 });
